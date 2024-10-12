9ee5eb6582bde7580c5b0aed7aac328db60fb4f0
raytriple7 <raytriple7@yahoo.com>
2024-10-13 04:55:24 +1100

    modify tf script

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vprofileactions23"
+    bucket = "vprofile-kop-bucket2311"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-southeast-2"
   }
 
   required_version = "~> 1.6.3"
